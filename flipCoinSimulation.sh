b9cb243829042d05409529c5968aa027c4bbbe2e
Tejassonar <sonartejas908@gmail.com>
2021-03-27 14:55:30 +0530

    Added : win by 21

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
-MAX_COUNT=10
+MAX_COUNT=50
 FLIP_COUNT=0
 HEAD=0
 TAILS=0
-while [ $FLIP_COUNT != $MAX_COUNT ]
+
+while [ $FLIP_COUNT -lt $MAX_COUNT  ]
 do
    A=$((RANDOM%2))
-
 
    if [ $A -eq 1 ]
       then
@@ -15,13 +15,30 @@ do
    else
       ((TAILS++))
    fi
+	((FLIP_COUNT++))
 
-   ((FLIP_COUNT++))
+	if [ $HEAD -eq 21 ]
+		then
+		FLIP_COUNT=55
+	elif [ $TAILS -eq 21 ]
+		then
+		FLIP_COUNT=55
+	fi
 done
-echo HEAD=$HEAD
-echo TAILS=$TAILS
 
 
+if [ $HEAD -eq 21 ]
+	then
+	echo "Head is won by 21"
+	echo Tails=$TAILS
+elif [ $TAILS -eq 21 ]
+	then
+	echo "Tails is won by 21"
+	echo "Head = $HEAD"
+elif [ $HEAD -eq $TAILS ]
+	then
+	echo "Its a Tie"
+fi
 
 
 
